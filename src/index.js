022c947f540fda59ab51c112239d9b123991d245
Crysisun <sunqingyu1@gmail.com>
2018-10-10 15:59:05 -0700

    Implementing search bar.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCEy-JmrOHX9ppIcfKILphf5dalEePM5Js';
 
 const App = () => {
-    return <div>Hi!</div>;
-}
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
