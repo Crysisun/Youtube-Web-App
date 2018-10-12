be1e34e9075804178a54330164037bbd4e3c2954
Crysisun <sunqingyu1@gmail.com>
2018-10-12 02:00:08 -0700

    Implementing video list.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import youtubeSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCEy-JmrOHX9ppIcfKILphf5dalEePM5Js';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-};
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+        youtubeSearch({key: API_KEY, term: 'usc'}, (videos) => {
+            this.setState({ videos }); // videos : videos
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
